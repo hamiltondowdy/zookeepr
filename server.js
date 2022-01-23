30eea7b27e8396d591da20ba54526423a69e8b68
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-01-23 15:40:36 -0600

    refactor server

--- server.js
+++ server.js
@@ -1,54 +1,18 @@
-app.use(express.static('public'));
-
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals'); 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.get('/api/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/api/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-app.post('/api/animals', (req, res) => {
-
-    req.body.id = animals.length.toString();
-
-    if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } else {
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  }
-  });
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  console.log(`API server now on port ${PORT}!`);
 });
